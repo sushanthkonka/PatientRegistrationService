7c3c62660fa5107af4bd802013c2ca4e59150377
Sushanth Konka <skcgn@umsystem.edu>
2025-05-16 11:12:52 -0500

    Updated Patient model property with Text fields validations.

--- Models/Patient.cs
+++ Models/Patient.cs
@@ -2,7 +2,37 @@ using System.ComponentModel.DataAnnotations;
 
 public class Patient
 {
-     public string? Name { get; set; }
+    public string? First_Name { get; set; }
+    public string? Last_Name { get; set; }
+    public string? Phone_Number { get; set; }
+
+    public string? Street_Address { get; set; }
+
+    public string? City_Address { get; set; }
+
+    public string? State_Address { get; set; }
+
+     public string? Zip_Code { get; set; }
+
+      public string? Gender { get; set; }
+
+      public string? Marital_Status { get; set; }
+
+      public string? Insurance_Name { get; set; }
+
+
+
+
+
+     
+
+
+
+
+
+
+
+
     public int Id { get; set; }
     // [Required(ErrorMessage = "First name is required")]
     // public string? FirstName { get; set; }
